8513ca2494370ea6fe920a4dee5db1260792544a
navitha <navithareddy112@gmail.com>
2022-09-05 20:53:17 +0530

    [navitha] 1.PresentAndAbsent

---
+++ EmpWageUsingOops/PresentAndAbsent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageUsingOops
+{
+    internal class PresentAndAbsent
+    {
+        public static void CheckAttendence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+                
+        }
+    }
+}
---
+++ EmpWageUsingOops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageUsingOops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option 1.Checkattendence");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    PresentAndAbsent.CheckAttendence();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
